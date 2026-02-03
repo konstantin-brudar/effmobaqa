b25699d434d230cb4492627440ddfb40b299fc69
Konstantin Brudar <brudar@bk.ru>
2026-02-03 20:22:01 +0300

    Add more login tests

--- src/test/java/ru/effmob/aqa/tests/LoginTest.java
+++ src/test/java/ru/effmob/aqa/tests/LoginTest.java
@@ -3,24 +3,30 @@ package ru.effmob.aqa.tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.effmob.aqa.pages.LoginPage;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginTest {
     private WebDriver driver;
     private LoginPage loginPage;
+    private WebDriverWait wait;
 
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.saucedemo.com/");
         loginPage = new LoginPage(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
     @AfterEach
@@ -35,6 +41,37 @@ public class LoginTest {
         loginPage.enterUsername("standard_user");
         loginPage.enterPassword("secret_sauce");
         loginPage.clickLoginButton();
+        assertTrue(loginPage.isProductsPageDisplayed());
+    }
+
+    @Test
+    void loginWithWrongPassword() {
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("wrong_password");
+        loginPage.clickLoginButton();
+        assertEquals("Epic sadface: Username and password do not match any user in this service", loginPage.getErrorMessage());
+    }
+
+    @Test
+    void lockedOutUserLogin() {
+        loginPage.enterUsername("locked_out_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        assertEquals("Epic sadface: Sorry, this user has been locked out.", loginPage.getErrorMessage());
+    }
+
+    @Test
+    void loginWithEmptyFields() {
+        loginPage.clickLoginButton();
+        assertEquals("Epic sadface: Username is required", loginPage.getErrorMessage());
+    }
+
+    @Test
+    void performanceGlitchUserLogin() {
+        loginPage.enterUsername("performance_glitch_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        wait.until(ExpectedConditions.urlContains("/inventory.html"));
         assertTrue(loginPage.isProductsPageDisplayed());
     }
 }
